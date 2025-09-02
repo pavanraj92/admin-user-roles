54b9e799c8bea73971f87f7f77de15e2165e0eb2
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-09-02 12:03:01 +0530

    publish file chnages

--- src/UserRoleServiceProvider.php
+++ src/UserRoleServiceProvider.php
@@ -56,11 +56,14 @@ class UserRoleServiceProvider extends ServiceProvider
             
         $slug = $admin->website_slug ?? 'admin';
 
+        $routeFile = base_path('Modules/UserRoles/routes/web.php');
+        if (!file_exists($routeFile)) {
+            $routeFile = __DIR__ . '/routes/web.php'; // fallback to package route
+        }
+
         Route::middleware('web')
             ->prefix("{$slug}/admin") // dynamic prefix
-            ->group(function () {
-                $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-            });
+            ->group($routeFile);
     }
 
     public function register()
